a5309bb2abf5983544e4da0d8ff3e324513066aa
mparekh3 <54687903+mparekh3@users.noreply.github.com>
2020-11-22 22:03:11 -0500

    Identifier method created.
    Keyword method is completed and working. Identifier method has some issue and under progress.

---
+++ code/Paser.cpp
@@ -0,0 +1,68 @@
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <string>
+using namespace std;
+class Compiler {
+ private:
+  string source;
+
+ public:
+  void foundIdentifier(uint32_t start, uint32_t end);
+  void foundIntConst(uint32_t start, uint32_t end);
+  void foundLongConst(uint32_t start, uint32_t end);
+};
+class RXEng {
+ public:
+  bool CheckKeyWord(string s) {
+    fstream key;
+    string word;
+    bool match;
+    key.open("KeyWords.txt");
+    while (key >> word) {
+      regex e(word);
+      match = regex_match(s, e);
+      if (match) return match;
+    }
+    return false;
+  }
+  string identifier(string s) {
+    regex r("[a-zA-Z_][a-zA-Z_0-9]*");
+    regex_token_iterator<string::iterator> rend;
+    regex_token_iterator<string::iterator> a(s.begin(), s.end(), r);
+    while (!(a == rend)) {
+      return a->str();
+    }
+  }
+};
+
+int main() {
+  string fileName;
+  cout << "Enter File Name:";
+  cin >> fileName;
+  fstream input;
+  input.open(fileName);
+  if (input.is_open())
+    cout << "file is open." << endl;
+  else {
+    cout << "file not found." << endl;
+    return 1;
+  }
+  RXEng r;
+  int x = 0;
+  while (!input.eof()) {
+    string temp;
+    input >> temp;
+    string temp2 = r.identifier(temp);
+    if (r.CheckKeyWord(temp)) {
+      cout << temp << '\n';
+      x++;
+    }
+    /* Identifier is under progress...
+     if (temp2.length() > 0 && !(r.CheckKeyWord(temp))) {
+       cout << "Identifier-->" << temp2 << endl;
+       }
+
+   */
+  }
+}
