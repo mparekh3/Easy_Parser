19b5452eedbe393d06edd2a7f29757157926715c
tparekh1 <70347216+tparekh1@users.noreply.github.com>
2020-11-30 20:28:34 -0500

    Update Paser.cpp

--- code/Paser.cpp
+++ code/Paser.cpp
@@ -1,68 +1,94 @@
-#include <fstream>
-#include <iostream>
-#include <regex>
-#include <string>
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<regex>
 using namespace std;
-class Compiler {
- private:
-  string source;
+class Compiler{
 
- public:
-  void foundIdentifier(uint32_t start, uint32_t end);
-  void foundIntConst(uint32_t start, uint32_t end);
-  void foundLongConst(uint32_t start, uint32_t end);
+private : 
+string source; 
+public : 
+void foundIdentifier ( uint32_t start , uint32_t end ) ; 
+void foundIntConst ( uint32_t start , uint32_t end ) ; 
+void foundLongConst ( uint32_t start , uint32_t end ) ; 
+
 };
-class RXEng {
- public:
-  bool CheckKeyWord(string s) {
-    fstream key;
-    string word;
-    bool match;
-    key.open("KeyWords.txt");
-    while (key >> word) {
-      regex e(word);
-      match = regex_match(s, e);
-      if (match) return match;
+class RXEng{
+public:
+    bool CheckKeyWord(string s){
+        fstream key;
+        string word;
+        bool match;
+        key.open("KeyWords.txt");
+        while(key>>word){
+        regex e(word) ;
+        match= regex_match(s,e);
+        if (match)
+            return match;
+        }
+       return false; 
     }
-    return false;
-  }
-  string identifier(string s) {
-    regex r("[a-zA-Z_][a-zA-Z_0-9]*");
-    regex_token_iterator<string::iterator> rend;
-    regex_token_iterator<string::iterator> a(s.begin(), s.end(), r);
-    while (!(a == rend)) {
-      return a->str();
+    bool CheckintCont(string s){
+        fstream key;
+        string word="-?[\\d]+";
+        bool match;
+        regex e(word) ;
+        match= regex_match(s,e);
+        if (match)
+            return match;
+        
+       return false; 
     }
-  }
+    bool CheckfloatCont(string s){
+        fstream key;
+        string word="-?[\\d]+\\.[\\d]{1,8}";
+        bool match;
+        regex e(word) ;
+        match= regex_match(s,e);
+        if (match)
+            return match;
+        
+       return false; 
+    }
 };
+int main(){
+    string fileName;
+    cout<<"Enter File Name:";
+    cin>>fileName;
+    fstream input;
 
-int main() {
-  string fileName;
-  cout << "Enter File Name:";
-  cin >> fileName;
-  fstream input;
-  input.open(fileName);
-  if (input.is_open())
-    cout << "file is open." << endl;
-  else {
-    cout << "file not found." << endl;
-    return 1;
-  }
-  RXEng r;
-  int x = 0;
-  while (!input.eof()) {
-    string temp;
-    input >> temp;
 
-    /* Identifier is not working completly */
-    if (r.CheckKeyWord(temp)) {
-      cout << "KeyWords: " << temp << '\n';
-      x++;
+    input.open(fileName);
+    char *b;
+    input.getline(b,input.tellg());
+    if(input.is_open())
+        cout<<"file is open."<<endl;
+    else{
+        cout<<"file not found."<<endl;
+        return 1;
     }
-    string temp2 = r.identifier(temp);
-    if (temp2.length() != 0 && temp2 != temp) {
-      cout << "Identifier:" << temp2 << endl;
+    RXEng r;
+    int x=0;
+    while(!input.eof()){
+        string temp;
+        input>>temp;
+        
+        if(r.CheckKeyWord(temp)){
+            cout<<"KeyWord:"<<temp<<'\n';
+            x++;
+        }
+        else if(r.CheckintCont(temp)){
+            cout<<"intCon:"<<temp<<'\n';
+            
+        }
+         else if(r.CheckfloatCont(temp)){
+            cout<<"floatCon:"<<temp<<'\n';
+            
+        }
     }
-  }
-  cout << "Total " << x << " KeyWords Found in file.\n";
+   for(int i=0;i<input.tellg();i++){
+       cout<<b[i]<<"  ";
+   }
+    
+
 }
