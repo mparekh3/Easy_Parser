1eb2714dd794a37b7b5b28a4338de4a9c4a601dd
mparekh3 <54687903+mparekh3@users.noreply.github.com>
2020-12-16 19:13:10 -0500

    Word by Word reading
    Found issue in regex function

---
+++ code/Sample_Study_Codes/sample_try3.cpp
@@ -0,0 +1,91 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <regex>
+#include <string>
+#include <vector>
+
+#include "comment.h"
+
+using namespace std;
+/*
+class Compiler {
+ private:
+  string source;
+ public:
+  void foundIdentifier(uint32_t start, uint32_t end) {}
+  void foundIntConst(uint32_t start, uint32_t end) {}
+  void foundLongConst(uint32_t start, uint32_t end) {}
+};
+*/
+class RXEng {
+ private:
+  // string source;
+
+ public:
+  string source;
+
+  bool CheckKeyWord(string s) {
+    fstream key;
+    string word;
+    smatch m;
+    key.open("KeyWords.txt");
+
+    while (key >> word) {
+      regex e(word);
+      return regex_search(s, m, e);
+    }
+    return false;
+  }
+
+  bool CheckintCont(string s) {
+    smatch m;
+    regex e("-?[\\d]+");
+    return regex_search(s, m, e);
+  }
+
+  bool CheckfloatCont(string s) {
+    smatch m;
+    regex e("-?[\\d]+\\.[\\d]{1,8}");
+    return regex_search(s, m, e);
+  }
+  bool CheckSpecialChar(string s) {
+    smatch m;
+    regex e("[^a-zA-Z0-9]+");
+    return regex_search(s, m, e);
+  }
+  bool CheckWhiteSpace(string s) {
+    smatch m;
+    regex e("[\\t\\r\\n]+");
+    return regex_search(s, m, e);
+  }
+};
+
+int main() {
+  string fileName = "testing.txt";
+  fstream input;
+  input.open(fileName.c_str());
+  string word;
+  map<string, vector<string>> token;
+  RXEng r;
+  while (input >> word) {
+    if (r.CheckKeyWord(word)) {
+      token["KeyWords"].push_back(word);
+    } else if (r.CheckSpecialChar(word)) {
+      token["floatConts"].push_back(word);
+    } else if (r.CheckintCont(word)) {
+      token["IntConts"].push_back(word);
+    } else if (r.CheckfloatCont(word)) {
+      token["floatConts"].push_back(word);
+    }
+  }
+  for (map<string, vector<string>>::const_iterator ptr = token.begin();
+       ptr != token.end(); ptr++) {
+    cout << ptr->first << ": ";
+    for (vector<string>::const_iterator eptr = ptr->second.begin();
+         eptr != ptr->second.end(); eptr++)
+      cout << *eptr << " ";
+    cout << endl;
+  }
+}
