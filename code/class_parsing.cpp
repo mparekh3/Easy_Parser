28a4fb29255a65ea23be27c117a0f5cc528ab1ae
mparekh3 <54687903+mparekh3@users.noreply.github.com>
2020-12-17 22:45:52 -0500

    Class Parsing code

---
+++ code/class_parsing.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <regex>
+
+using namespace std;
+
+int main() {
+    regex r( "class\\s+([a-zA-Z_][a-zA-Z0-9_]*)*\\s*:*\\s*((public|private) ([a-zA-Z_][a-zA-Z0-9_]*))*\\s*\\{");
+    ifstream f("hwregex");
+    char line[1000];
+	int i=1;
+    cmatch m;
+          
+    if(f.is_open()) {
+        while (f) {
+            f.getline(line,1000);
+	    if (regex_search (line,m,r)) {
+            		  cout <<i <<". CLASS_NAME_IS:  " <<m[1]<< "\t\t\t";
+		if(m[4]=="")
+			cout<<"Inheritance_Base_Name_Is: None"<<endl;
+		else cout<<"Inheritance_Base_Name_Is: "<<m[4]<<endl;
+		i++;
+            }
+        }
+    }
+}
